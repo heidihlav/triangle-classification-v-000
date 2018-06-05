e0b0834b4ab38775ea4bb59b0788ac2c0d899f71
Heidi Hlavinka <hchlav@gmail.com>
2018-06-05 15:38:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
 
   def real_triangle
     triangle = a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0
-    raise TriangleError.message if !triangle
+    raise TriangleError if !triangle
     # binding.pry
   end
 
